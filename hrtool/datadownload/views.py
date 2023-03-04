61be6e448d51d376a7e5c6a25e8965f8688da606
Olexander <afurmanenko@gmail.com>
2023-03-04 21:02:30 +0200

    add survcalc

--- hrtool/datadownload/views.py
+++ hrtool/datadownload/views.py
@@ -2,22 +2,19 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UploadFileForm
 from openpyxl import load_workbook
+import pandas as pd
+from . import surcalc
+
 
 # Create your views here.
 def download_view(request):
-    # if not request.user.is_authenticated:
-    #     context = {'message': 'Please login to receive an access to dashboards',}
-    #     return render(request, 'login.html', context)
-    # else:
-    #     return render(request, 'dl.html')
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            wb = load_workbook(filename=request.FILES['file'])
-            sheet_ranges = wb['Sheet1']
-            print(sheet_ranges['A2'].value)
-            # print(request.FILES['file'].read())
-            # form.save()
+            file = request.FILES['file']
+            data = pd.read_excel(file)
+            surcalc.dataStructure(data)#оновлюэмо файл
+            # print(data)
             return redirect('index')
     else:
         form = UploadFileForm()
